6d85933f10738acb413e2d4c87ce69acd8282e96
deepanshu verma <deepanshuverma186@gmail.com>
2025-08-24 16:16:11 +0530

    add profile picture upload with preview in register form

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,31 +1,79 @@
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-        <div className=" flex justify-evenly h-[90vh] items-center">
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-       <div className="avatar flex justify-center">
-  <div className="ring-primary ring-offset-base-100 w-16 rounded-full ring-2 ring-offset-2">
-    <img className="" src="https://img.daisyui.com/images/profile/demo/spiderperson@192.webp" />
-  </div>
-</div>
-      <label className="label">Name</label>
-  <input type="text" className="input" placeholder="Name" />
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profilePic: null as File | null,
+  });
+
+  const [preview, setPreview ] = useState<string | null>(null) 
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target
+     if(name === 'profilePic' && files){
+       setUser({...user, profilePic:files[0]})
+       setPreview(URL.createObjectURL(files[0]))
+     }else{
+      setUser({...user,[name]:value})
+     }
+  };
   
-  <label className="label">Email</label>
-  <input type="email" className="input" placeholder="Email" />
+  function handleSubmit(){
+    console.log(user)
+  }
 
-  <label className="label">Password</label>
-  <input type="password" className="input" placeholder="Password" />
+  return (
+    <div className=" flex justify-evenly h-[90vh] items-center">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <div className="avatar flex justify-center">
+          <div className="ring-primary ring-offset-base-100 w-16 rounded-full ring-2 ring-offset-2">
+            {
+              preview &&  <img
+              className=""
+              src={preview}
+            />
+            }
+          </div>
+        </div>
+        <label className="label">Name</label>
+        <input type="text"
+        name="name" 
+        className="input"
+        onChange={handleChange} 
+        placeholder="Name" />
 
-   <label className="label">Profile Pic</label>
-     <input type="file" className="file-input" />
-  <button className="btn btn-neutral mt-4">Login</button>
-</fieldset>
+        <label className="label">Email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          className="input"
+          placeholder="Email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          name="password"
+          className="input"
+          onChange={handleChange}
+          placeholder="Password"
+        />
+
+        <label className="label">Profile Pic</label>
+        <input 
+        type="file" 
+        name="profilePic"
+        onChange={handleChange} 
+        className="file-input" />
+        <button onClick={handleSubmit}  className="btn btn-neutral mt-4">
+          Create Profile
+        </button>
+      </fieldset>
     </div>
-  )
-}
+  );
+};
 
-export default Register
-
-
-
+export default Register;
